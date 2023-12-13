e0e954dd5240f102e012bcd83a2f3f5f90dff0bc
DraganGlamocic <gla.dragan@gmail.com>
2023-12-13 12:55:04 +0100

    Practice: # Component in app.component.ts. # Template Syntax in angular.json, changed prefix from "app" to "hinv". also in app.component.ts and index.html

--- projects/hotelinventory/src/app/app.component.ts
+++ projects/hotelinventory/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // template: `<h1>Hello world from inline template.</h1>
+  // <h2><p>Testing multiline using backtick.</p></h2>
+  // `,
+  styleUrls: ['./app.component.scss'],
+  styles: [`h1 { color: #ffbbff}`, `h2 { color: #ffdd00}`]
 })
 export class AppComponent {
   title = 'hotelinventory';
